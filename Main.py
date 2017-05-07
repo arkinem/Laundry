6ffede8f8f29ea387d37f4bc24235f90232b34d9
Goliblazej <blazejgolinski@gmail.com>
2017-05-07 18:13:26 +0100

    huj

---
+++ Main.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from functools import partial
+
+
+class MainWindow():
+	def __init__(self):
+		root = tk.Tk()
+		root.geometry("300x200")
+		root.title("Login")
+
+		# self.window.geometry("290x180")
+
+		self.LabelLogin = tk.Label(text="Login: ")
+		self.LabelLogin.grid(row=0, column="0")
+
+		self.EntryLogin = tk.Entry(width="30")
+		self.EntryLogin.grid(row=0, column="1")
+
+		self.LabelPassword = tk.Label(text="Password : ")
+		self.LabelPassword.grid(row=1, column="0")
+
+		self.EntryPassword = tk.Entry(width="30")
+		self.EntryPassword.grid(row=1, column="1")
+
+		self.ButtonLogin = tk.Button(root, text="Login", width=10, height=1)
+		self.ButtonLogin.grid(row=2, column="1")
+
+		self.ButtonRegister = tk.Button(root, text="Register", width=10, height=1, command=self.registerWindow)
+		self.ButtonRegister.grid(row=2, column="0")
+
+		root.bind('<Return>', self.func())
+
+		root.mainloop()
+
+	def func(event):
+		print("You hit return.")
+
+	def registerWindow(self):
+		print("You clicked the button")
+		registerWindow = tk.Toplevel()
+		registerWindow.title("Register")
+		registerWindow.grab_set()
+
+		self.LabelName = tk.Label(registerWindow, text="Name: ")
+		self.LabelName.grid(row=0, column="0")
+
+		self.EntryName = tk.Entry(registerWindow, width="30")
+		self.EntryName.grid(row=0, column="1")
+
+		self.LabelLogin = tk.Label(registerWindow, text="Login : ")
+		self.LabelLogin.grid(row=1, column="0")
+
+		self.EntryLogin = tk.Entry(registerWindow, width="30")
+		self.EntryLogin.grid(row=1, column="1")
+
+		self.LabelPassword = tk.Label(registerWindow, text="Password : ")
+		self.LabelPassword.grid(row=2, column="0")
+
+		self.EntryPassword = tk.Entry(registerWindow, width="30")
+		self.EntryPassword.grid(row=2, column="1")
+
+		self.ButtonRegister = tk.Button(registerWindow, text="Register", width=10, height=1, command=self.registerWindow)
+		self.ButtonRegister.grid(row=3, column="1")
+
+
+
+
+app = MainWindow()
+
+
